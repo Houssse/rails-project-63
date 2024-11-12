6fb7985e8b8a86a22ed07f57d420f10a4dba77ff
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-11-13 02:24:46 +0300

    the second stage of the project

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,25 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Tag
+    def self.build(tag_name, **options)
+      options_string = options.map { |key, value| "#{key}=\"#{value}\"" }.join(' ')
+  
+      if block_given?
+        content = yield
+        if options.empty? 
+          return "<#{tag_name}>#{content}</#{tag_name}>" 
+        else
+          return "<#{tag_name} #{options_string}>#{content}</#{tag_name}>"
+        end
+      end
+
+      if options.empty?
+        tag_name == 'br' ? "<#{tag_name}>" : "<#{tag_name}></#{tag_name}>" 
+      else
+        "<#{tag_name} #{options_string}>"
+      end
+    end
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
+  def test_build_br_tag
+    result = HexletCode::Tag.build('br')
+    assert_equal '<br>', result
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_build_img
+    result = HexletCode::Tag.build('img', src: 'path/to/image')
+    assert_equal "<img src=\"path/to/image\">", result
+  end
+
+  def test_build_input
+    result = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+    assert_equal "<input type=\"submit\" value=\"Save\">", result
+  end
+
+  def test_build_label
+    result = HexletCode::Tag.build('label') { 'Email' }
+    assert_equal "<label>Email</label>", result
+  end
+
+  def test_build_label_options
+    result = HexletCode::Tag.build('label', for: 'email') { 'Email' }
+    assert_equal "<label for=\"email\">Email</label>", result
+  end
+
+  def test_build_div_tag
+    result = HexletCode::Tag.build('div')
+    assert_equal "<div></div>", result
   end
 end
