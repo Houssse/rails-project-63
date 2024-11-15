4053b27c44c671ef069ba9cd953d919bfe353f71
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-11-15 03:14:55 +0300

    completion of the project

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -29,29 +29,40 @@ module HexletCode
     end
 
     def input(attribute, **options) # rubocop:disable Metrics/MethodLength
-      begin
-        value = @object.public_send(attribute)
-      rescue NoMethodError
-        raise HexletCode::Error, "undefined method `#{attribute}' for #<struct User id=nil, name=nil, job=nil>"
-      end
+      value = fetch_attribute_value(attribute)
 
-      label_text = attribute.to_s.capitalize
-      input_id = "input_#{attribute}"
+      build_label(attribute)
 
-      @form_content += Tag.build("label", for: input_id) { label_text }
       if options[:as] == :text
         options[:cols] ||= 20
         options[:rows] ||= 40
-        @form_content += Tag.build("textarea", name: attribute, id: input_id, cols: options[:cols],
+        @form_content += Tag.build("textarea", name: attribute,
+                                               id: "input_#{attribute}",
+                                               cols: options[:cols],
                                                rows: options[:rows]) { value }
       else
-        @form_content += Tag.build("input", name: attribute, id: input_id, type: "text", value: value, **options)
+        @form_content += Tag.build("input", name: attribute,
+                                            id: "input_#{attribute}",
+                                            type: "text",
+                                            value: value, **options)
       end
     end
 
     def submit(value: "Save")
       @submit_added = true
       @form_content += Tag.build("input", type: "submit", value: value)
+    end
+
+    def build_label(attribute)
+      label_text = attribute.to_s.capitalize
+      input_id = "input_#{attribute}"
+      @form_content += Tag.build("label", for: input_id) { label_text }
+    end
+
+    def fetch_attribute_value(attribute)
+      @object.public_send(attribute)
+    rescue NoMethodError
+      raise HexletCode::Error, "undefined method `#{attribute}' for #<struct User id=nil, name=nil, job=nil>"
     end
 
     attr_reader :form_content, :submit_added
